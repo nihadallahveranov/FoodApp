7eb53d5d29f9d27bdba20096558068a10767bc86
Nihad Allahveranov <nihad.allahveranov@gmail.com>
2022-12-12 20:30:12 +0400

    Initial Commit

---
+++ FoodApp/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  FoodApp
+//
+//  Created by Nihad Allahveranov on 12.12.22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
